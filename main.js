2a957c8b028dbb1d65a9432f51fe413e9444170b
Tomas <t.r.hillside@gmail.com>
2023-01-23 21:19:32 +0000

    Added slight logic to alter the DOM when clicking on an empty Tic Tac Toe square

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
 /* eslint-disable no-plusplus */
-const boardGenerator = () => {
-  const GAME_BOARD = document.getElementById('game-board');
+(function () {
+  const initialRender = () => {
+    // cache DOM
+    const board = document.getElementById('game-board');
 
-  for (let i = 0; i < 9; i++) {
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    GAME_BOARD.appendChild(square);
-  }
-};
+    // Render
+    function render() {
+      if (board.children.length === 9) {
+        return;
+      }
+      for (let i = 0; i < 9; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        board.appendChild(square);
+      }
+    }
 
-boardGenerator();
+    board.classList.toggle('hidden');
+
+    return { board, render };
+  };
+
+  const newBoard = initialRender();
+  newBoard.render();
+  newBoard.board.classList.toggle('hidden');
+
+  const ticTacToe = () => {
+    // Cache DOM
+    const exClass = 'x';
+    const circleClass = 'O';
+    const squares = document.querySelectorAll('.square');
+    let xTurn;
+
+    function drawMark(square, currentClass) {
+      square.innerHTML = currentClass;
+    }
+
+    function squareHandler(e) {
+      const square = e.target;
+      const currentClass = xTurn ? circleClass : exClass;
+      drawMark(square, currentClass);
+    }
+
+    // Listeners
+    squares.forEach((square) => {
+      square.addEventListener('click', squareHandler, { once: true });
+    });
+  };
+  ticTacToe();
+}());
